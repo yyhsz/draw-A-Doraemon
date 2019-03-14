812027086d9cf6a52fe3a1e3f526a46ce5b7bf0f
yyhsz <591985864@qq.com>
2019-03-14 17:54:06 +0800

    完成一个带有调速功能，展现制作动画过程的页面

--- main.js
+++ main.js
@@ -1,27 +1,74 @@
 {
+    let duration = 50
     let writeCode = function (prefix, code, callback) {
         let styleTag = document.querySelector('#styleTag')
         let container = document.querySelector('#code')
         let codeWrapper = document.querySelector('.code-wrapper')
-        styleTag.innerHTML = ''
-        container.innerHTML = ''
+        styleTag.innerHTML = prefix
         let n = 0
-        let id = setInterval(() => {
+        function fn (){
             styleTag.insertAdjacentHTML('beforeend', code[n])
-            container.insertAdjacentHTML('beforeend', code[n])
+            container.innerHTML = prefix + Prism.highlight(code.substring(0,n), Prism.languages.css, 'css');
             codeWrapper.scrollTop = 10000
             n++
-            if (n >= code.length) {
-                clearInterval(id)
+            if (n < code.length) {
+                setTimeout(fn,duration)
+            }else{
                 callback()
             }
-        }, 1);
+        }
+        setTimeout(fn,duration)
     }
-    let code = `
+
+   $('.action a').on('click',(x)=>{
+       $(x.currentTarget).addClass('active')
+        .siblings().removeClass('active')
+        let speed  = $(x.currentTarget).text()
+        switch(speed){
+            case '慢速':
+            duration = 100
+            break
+            case '中速':
+            duration = 40
+            break
+            case '快速':
+            duration = 10
+        }
+   })
+
+
+    let prefix = `
+    /*
+    我想画一只哆啦A梦
+    首先准备一些样式
+    */
+    .code-wrapper{
+        background: rgb(250, 232, 232);
+    }
     .preview{
         position: relative;width:100%;height: 100%;
-        border:1px solid black;
+        border:1px solid rgb(216, 38, 135);
+        box-shadow:  0 0 3px 3px rgba(0, 0, 0, 0.2)
     }
+    /*等等，这代码的颜色太丑了，让我高亮一下*/
+    .token.selector{
+        color:#690 ;
+    }
+    .token.punctuation{
+        color:#999 ;    
+    }
+    .token.property{
+        color:#905 ;
+    }
+    .token.function{
+        color:#DD4A68;
+    }
+    /*好了，准备工作完成，我开始了*/
+    `
+    let code = `
+    
+    /*首先是蓝蓝的大头*/
+    
     .head{
         overflow: hidden;
         height: 280px;width:280px;
@@ -30,19 +77,25 @@
         border-radius:200px/216px;
         position: absolute;top:35px;left:50%;margin-left: -140px;
     }
+   
+    /*白白的脸蛋*/
+    
     .face{
-          overflow: hidden;
+        overflow: hidden;
         width:246px;height:246px;
         background: white;
         border-radius:50%;
-        position: absolute;top:70px;left:50%;margin-left:-123px;
         border:2px solid black;
+        position: absolute;top:70px;left:50%;margin-left:-123px;
     }
+    
+    /*然后是笑眯眯的眼睛*/
+   
     .eye{
         width:60px;height:60px;
         border-radius:50%;background: white;
-        position: absolute;top:45px;
         border:2px solid black;
+        position: absolute;top:45px;
     }
     .eye.right{
         left:50%;
@@ -66,11 +119,14 @@
     .eye.left::after{
         left:50%;
     }
+    
+    /*红彤彤的鼻子*/
+   
     .nose{
         width:36px;height:36px;
         border-radius:50%;background: rgb(254, 0, 0);
-        position: absolute;top:94px;left:50%;margin-left:-18px;
         border:2px solid black;
+        position: absolute;top:94px;left:50%;margin-left:-18px;
     }
     .nose::after{
         content:'';display: block;
@@ -86,7 +142,6 @@
         position: absolute;top:130px;left:50%;
     }
     .lip.left{
-        position: absolute;top:124px;left:50%;margin-left: -120px;
         border:2px solid black;
         border-right:transparent;
         border-top:transparent;
@@ -94,9 +149,9 @@
         background: #fff;
         border-bottom-left-radius:33px 30px;
         transform:rotate(-8deg);
+        position: absolute;top:124px;left:50%;margin-left: -120px;
     }
     .lip.right{
-        position: absolute;top:124px;right:50%;margin-right: -120px;
         border:2px solid black;
         border-left:transparent;
         border-top:transparent;
@@ -104,24 +159,24 @@
         background: #fff;
         border-bottom-right-radius:33px 30px;
         transform:rotate(8deg);
+        position: absolute;top:124px;right:50%;margin-right: -120px;
     }
     .lip.left::after{
         content:'';display: block;
         height:20px;width:38px;
-        position: absolute;top:-51%;right:69.4%;
         border:2px solid black;
         border-right:transparent;
         border-bottom:transparent;
         border-top-left-radius:20px 20px;
         transform:rotate(0deg);
         background: #fff;
+        position: absolute;top:-51%;right:69.4%;
     }
     .lip.right::after{
         content: '';
         display: block;
         height: 20px;
         width: 38px;
-        position: absolute;
         top: -51%;
         left: 69.4%;
         border: 2px solid black;
@@ -130,7 +185,11 @@
         border-top-right-radius: 20px 20px;
         transform: rotate(0deg);
         background: #fff;
+        position: absolute;
     }
+   
+    /*猫咪怎么能少了胡须*/
+    
     .beard{
         height: 1px;
         width: 57px;
@@ -169,50 +228,62 @@
         right: 50%;margin-right:-90px;
         transform: rotate(10deg);
     }
+   
+    /*最后是血盆大口*/
+    
     .mouse{
         height: 700px;
         width: 200px;
         border-radius: 270px/378px;
-        position: absolute;
         top: -576px;
         left: 50%;
         margin-left: -100px;
         background: rgb(254, 0, 0);
         border: 2px solid black;
+        position: absolute;
     }
     .mouse-wrapper{
         height: 140px;width: 230px;
-        position: absolute;top:155px;left: 50%;margin-left: -115px;
         overflow: hidden;
+        position: absolute;top:155px;left: 50%;margin-left: -115px;
     }
+    
+    /*当然里面得有舌头*/
+    
     .tongue{
-        position: absolute;top:236px;left:50%;margin-left: -40px;
         width: 80px;height: 40px;
         border: 2px solid black;
         border-top:transparent;
         z-index:2;
         border-radius: 0 0 100px 100px;
         background: rgb(255, 110, 55);
+        position: absolute;top:236px;left:50%;margin-left: -40px;
     }
     .tongue::after{
         content: '';display: block;
         height: 30px;width: 30px;
-        position: absolute;top:-40%;left:6%;
         border: 2px solid black;
         border-left:transparent;border-bottom: transparent;
         border-top-right-radius:89px 64px;
         transform: rotate(-45deg);
         background: rgb(255, 110, 55);
+        position: absolute;top:-40%;left:6%;
     }
     .tongue::before{
         content: '';display: block;
         height: 30px;width: 30px;
-        position: absolute;top:-40%;right:5%;
         border: 2px solid black;
         border-right:transparent;border-bottom: transparent;
         border-top-left-radius:89px 64px;
         transform: rotate(45deg);
         background: rgb(255, 110, 55);
-    }`
-    writeCode('',code)
+        position: absolute;top:-40%;right:5%;
+    }
+    
+    /*这只哆啦A梦就送给你*/    
+    `
+    writeCode('',prefix,()=>{
+        writeCode(prefix,code,()=>{
+        })
+    })
 }   
